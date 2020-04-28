8c1ad3f471f8e18b2e17908c9ac8692b0ddf2388
bret-gibson <bret.gibson@gmail.com>
2020-04-28 18:16:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+document.addEventListener("DOMContentLoaded", () => {
+    // submitData("name", "email")
+}) 
+const submitData = (userName, userEmail) => {
+
+    const obj = {
+        name: userName,
+        email: userEmail,
+    }
+
+     return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(obj)
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        document.body.innerHTML = data["id"]
+    })
+    .catch(error => document.body.innerHTML = error.message)
+}
